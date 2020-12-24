bf639e115637190f2baff7551988537ae7d9a612
Kathy <psh09018@gmail.com>
2020-12-25 03:02:50 +0800

    first test

--- app.py
+++ app.py
@@ -54,29 +54,30 @@ parser = WebhookParser(channel_secret)
 
 @app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    return "hello world!"
+    # signature = request.headers["X-Line-Signature"]
+    # # get request body as text
+    # body = request.get_data(as_text=True)
+    # app.logger.info("Request body: " + body)
 
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+    # # parse webhook body
+    # try:
+    #     events = parser.parse(body, signature)
+    # except InvalidSignatureError:
+    #     abort(400)
 
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
+    # # if event is MessageEvent and message is TextMessage, then echo text
+    # for event in events:
+    #     if not isinstance(event, MessageEvent):
+    #         continue
+    #     if not isinstance(event.message, TextMessage):
+    #         continue
 
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
+    #     line_bot_api.reply_message(
+    #         event.reply_token, TextSendMessage(text=event.message.text)
+    #     )
 
-    return "OK"
+    # return "OK"
 
 
 @app.route("/webhook", methods=["POST"])
