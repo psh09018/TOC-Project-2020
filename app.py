fd4ac7014708593418e9532f93e518bc8d8d0db2
Kathy <psh09018@gmail.com>
2020-12-26 20:04:23 +0800

    test

--- app.py
+++ app.py
@@ -1,130 +1,47 @@
 import os
-import sys
+import json
 
-from flask import Flask, jsonify, request, abort, send_file
+import requests
 from dotenv import load_dotenv
-from linebot import LineBotApi, WebhookParser
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from flask import Flask, request, jsonify, send_from_directory
 
+from helper import LineAPI, webhook_parser
 from fsm import TocMachine
-from utils import send_text_message
+
 
 load_dotenv()
+app = Flask(__name__, static_url_path='')
+machines = {}
 
 
-machine = TocMachine(
-    states=["user", "state1", "state2", "state3"],
-    transitions=[
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
-        },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
-        },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state3",
-            "conditions": "is_going_to_state3",
-        },
-        {"trigger": "go_back", "source": ["state1", "state2", "state3"], "dest": "user"},
-    ],
-    initial="user",
-    auto_transitions=False,
-    show_conditions=True,
-)
+# Handle State Trigger
+def handleTrigger(state, reply_token, user_id, text):
+    print("Server Handling State : %s" % state)
+    if state == "init":
+        machines[user_id].advance(reply_token, text)
+    if state == "options":
+        machines[user_id].choose_options(reply_token, text)
+    if state == "summation":
+        machines[user_id].enter_number(reply_token, text)
 
-app = Flask(__name__, static_url_path="")
+@app.route('/', methods=['GET'])
+def reply():
+    return 'Hello, World!'
 
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
-channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
-if channel_secret is None:
-    print("Specify LINE_CHANNEL_SECRET as environment variable.")
-    sys.exit(1)
-if channel_access_token is None:
-    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
-    sys.exit(1)
+@app.route('/', methods=['POST'])
+def receive():
+    webhook = json.loads(request.data.decode("utf-8"))
+    reply_token, user_id, message = webhook_parser(webhook)
+    print(reply_token, user_id, message)
 
-line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
+    if user_id not in machines:
+        machines[user_id] = TocMachine()
 
-
-# @app.route("/callback", methods=["POST"])
-# def callback():
-#     ##return "hello world!"
-#     signature = request.headers["X-Line-Signature"]
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-
-#     # parse webhook body
-#     try:
-#         events = parser.parse(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-
-#     # if event is MessageEvent and message is TextMessage, then echo text
-#     for event in events:
-#         if not isinstance(event, MessageEvent):
-#             continue
-#         if not isinstance(event.message, TextMessage):
-#             continue
-
-#         if event.message.text == 'go to state1':
-#             line_bot_api.reply_message(event.reply_token, TextSendMessage(text='do something for state1'))
-
-#         # line_bot_api.reply_message(
-#         #     event.reply_token, TextSendMessage(text='event.message.text')
-#         # )
-
-#     return "OK"
+    handleTrigger(machines[user_id].state, reply_token, user_id, message)
+    return jsonify({})
 
 
-@app.route("/webhook", methods=["POST"])
-def webhook_handler():
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info(f"Request body: {body}")
-
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-        if not isinstance(event.message.text, str):
-            continue
-        print(f"\nFSM STATE: {machine.state}")
-        print(f"REQUEST BODY: \n{body}")
-        response = machine.advance(event)
-        if response == False:
-            send_text_message(event.reply_token, "Not Entering any State")
-
-    return "OK"
-
-
-@app.route("/show-fsm", methods=["GET"])
-def show_fsm():
-    machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
-
-
-if __name__ == "__main__":
-    port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port = port)
