b6b9ea14f584475ca3f23ae1916803ce717ed341
Kathy <psh09018@gmail.com>
2020-12-27 17:15:33 +0800

    test

--- fsm.py
+++ fsm.py
@@ -1,6 +1,6 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message, send_image_message
+from utils import send_text_message, send_image_message, send_mix_message
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
